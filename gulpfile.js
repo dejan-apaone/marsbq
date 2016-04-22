1e9b53f6f049dde36eceae375ced971c7ae457fc
Dejan <dejan@apaone.com>
2016-04-22 08:47:40 +0200

    started with browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 
 gulp.task('default', function(){
-		console.log('Your first task has run!');
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
+	// gulp.watch("src/**/*", browserSync.reload);
+	gulp.watch("src/**/*").on('change', browserSync.reload);
+	gulp.watch("*.html").on('change', browserSync.reload);
 
 });
