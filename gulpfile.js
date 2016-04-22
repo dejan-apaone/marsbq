061b8f38726ed518fb7f22fc4a500716f3d150cb
Dejan <dejan@apaone.com>
2016-04-22 09:37:19 +0200

    implement gulp minification tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,36 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
+var uglyfly = require('gulp-uglyfly');
+const imagemin = require('gulp-imagemin');
+
+gulp.task('images', () => {
+	return gulp.src('src/img/**/*')
+	.pipe(imagemin())
+	.pipe(gulp.dest('dist/img'))
+	.pipe(browserSync.stream());
+});
+
+
+gulp.task('styles', function() {
+	return gulp.src('src/styles/*.css')
+	.pipe(sourcemaps.init())
+	.pipe(cleanCSS({compatibility: 'ie8'}))
+	.pipe(sourcemaps.write('../maps'))
+	.pipe(gulp.dest('dist/styles'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('scripts', function() {
+	gulp.src('src/scripts/main.js')
+	.pipe(sourcemaps.init())
+	.pipe(uglyfly())
+	.pipe(sourcemaps.write('../maps'))
+	.pipe(gulp.dest('dist/scripts'))
+	.pipe(browserSync.stream());
+});
+
 
 gulp.task('default', function(){
 	browserSync.init({
@@ -7,8 +38,10 @@ gulp.task('default', function(){
 			baseDir: "./"
 		}
 	});
-	// gulp.watch("src/**/*", browserSync.reload);
-	gulp.watch("src/**/*").on('change', browserSync.reload);
+	// gulp.watch("src/**/*").on('change', browserSync.reload);
+	gulp.watch("src/img/**/*", ['images']);
+	gulp.watch("src/styles/**/*.css", ['styles']);
+	gulp.watch("src/scripts/**/*.js", ['scripts']);
 	gulp.watch("*.html").on('change', browserSync.reload);
 
 });
